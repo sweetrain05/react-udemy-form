40dcdf74999425b6a2dd039882b483b9618bff2a
Danbi Choi <sweetrain05@hotmail.com>
2023-11-29 11:20:00 -0800

    react form basics via state & generic handlers

---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+export default function Login() {
+    const [enteredValues, setEnteredValues] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(enteredValues);
+    };
+
+    const handleInputChange = (identifier, value) => {
+        setEnteredValues((prev) => ({ ...prev, [identifier]: value }));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+
+            <div className='control-row'>
+                <div className='control no-margin'>
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        id='email'
+                        type='email'
+                        name='email'
+                        onChange={(e) =>
+                            handleInputChange('email', e.target.value)
+                        }
+                        value={enteredValues.email}
+                    />
+                </div>
+
+                <div className='control no-margin'>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        id='password'
+                        type='password'
+                        name='password'
+                        onChange={(e) =>
+                            handleInputChange('password', e.target.value)
+                        }
+                        value={enteredValues.password}
+                    />
+                </div>
+            </div>
+
+            <p className='form-actions'>
+                <button className='button button-flat'>Reset</button>
+                <button className='button'>Login</button>
+            </p>
+        </form>
+    );
+}
