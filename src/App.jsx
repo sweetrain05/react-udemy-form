0b5b2416c46e2e5ba60c7940b6078ef3e9153be6
Danbi Choi <sweetrain05@hotmail.com>
2023-11-29 19:57:57 -0800

    Mixing Custom & Built-in Validation Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+//import Login from './components/Login.jsx';
 //import StateLogin from './components/StateLogin.jsx';
-//import Signup from './components/Signup';
+import Signup from './components/Signup';
 
 function App() {
     return (
         <>
             <Header />
             <main>
-                <Login />
-                {/* <Signup /> */}
+                <Signup />
             </main>
         </>
     );
